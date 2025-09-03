a0a37d188975c69ce08c58ea2ab6f79e5fb7d367
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-09-03 23:16:06 +0200

    fix(nerdfonts): add unzip command after download

--- nerdfonts.sh
+++ nerdfonts.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/bash
 declare nf_download_url="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/"
 declare -a nf_list=(
-	Hack.zip
-	JetBrainsMono.zip
-	Meslo.zip
-	Monaspace.zip
+	Hack
+	JetBrainsMono
+	Meslo
+	Monaspace
 )
 
 if [ -d $HOME/.local/share/fonts ]; then
 	echo "Download nerd fonts"
 	for nf in "${nf_list[@]}"; do
-		curl --output-dir $HOME/.local/share/fonts -OL $nf_download_url$nf
+		curl --output-dir $HOME/.local/share/fonts -OL $nf_download_url${nf}.zip
+		echo "Extracting ${nf}.zip"
+		unzip $HOME/.local/share/fonts/${nf}.zip -d $HOME/.local/share/$nf
 	done
 fi

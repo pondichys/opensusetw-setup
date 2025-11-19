2b9393bdbe97bdb467dd7434e409620c3afabc17
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-11-19 19:06:33 +0100

    fix(nerdfonts): extract font archive to correct target folder and delete them afterwards

--- nerdfonts.sh
+++ nerdfonts.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/bash
+declare local_fonts_dir="$HOME/.local/share/fonts"
 declare nf_download_url="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/"
 declare -a nf_list=(
 	Hack
 	JetBrainsMono
 	Meslo
-	Monaspace
 )
 
-if [ -d $HOME/.local/share/fonts ]; then
-	echo "Download nerd fonts"
-	for nf in "${nf_list[@]}"; do
-		curl --output-dir $HOME/.local/share/fonts -OL $nf_download_url${nf}.zip
-		echo "Extracting ${nf}.zip"
-		unzip $HOME/.local/share/fonts/${nf}.zip -d $HOME/.local/share/$nf
-	done
+if [ ! -d "$local_fonts_dir" ]; then
+	mkdir -pv "$local_fonts_dir"
 fi
+
+echo "Downloading nerd fonts"
+for nf in "${nf_list[@]}"; do
+	curl --output-dir "$local_fonts_dir" -OL "$nf_download_url${nf}.zip"
+	echo "Extracting ${nf}.zip"
+	unzip "$local_fonts_dir/${nf}.zip" -d "$local_fonts_dir/$nf"
+	echo "Removing ${nf}.zip archive"
+	rm -v "$local_fonts_dir/${nf}.zip"
+done
